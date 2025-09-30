54609242b6689772b9f192db85f92ec07c41b7f0
Muhammad Nouman <mnouman2025o@gmail.com>
2025-09-30 18:20:04 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react() ,   tailwindcss(),
 ],
+    resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+
 })
